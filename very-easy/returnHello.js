4f98749e167dcf88268c06da9f9dc643685c7f3c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-21 11:18:20 -0500

    :art: Update

---
+++ very-easy/returnHello.js
@@ -0,0 +1,6 @@
+function hello() {
+  return "Hello";
+}
+
+// Test
+console.log(hello());
---
+++ very-easy/returnSumTwoNumbers.js
@@ -0,0 +1,6 @@
+function addition(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
+// Test
+console.log(addition(8, 9));

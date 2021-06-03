c64cb3efac8b87af5fbd573ade7b72ddfde7f403
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-03 10:41:31 -0500

    :sparkles: JavaScripting Day 2

---
+++ javascripting/02-variables.js
@@ -0,0 +1,9 @@
+let anExample;
+//  The above variable is declared, but it isn't defined (it doesn't yet reference a specific value)
+
+//  Here's an example of defining a variable, making it reference a specific value:
+const exampleTwo = 'a string';
+
+const example = 'some string';
+
+console.log(example);
